539dec2da9f488a898d8e809f5d59515e3da94f0
unknown <kalilulekan@gmail.com>
2023-07-02 22:28:34 +0100

    emoji

--- App.js
+++ App.js
@@ -2,12 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,  View  } from 'react-native';
 import ImageSource from './components/ImageSource';
 import Button from './components/button';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
 import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react';
-
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const [pickedEmoji, setPickedEmoji] = useState(null)
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false)
   const [selectedImage, setSelectedImage] = useState(null)
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -17,9 +24,23 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(True)
     } else {
       alert('You did not select any image.');
     }
+  };
+  const onReset = ()=>{
+    setShowAppOptions(false)
+  }
+  const onAddSticker = () => {
+    setIsModalVisible(true)
+  };
+
+  const onSaveImageAsync = async () => {
+ 
+  };
+  const onModalClose = () =>{
+    setIsModalVisible(false)
   };
   return (
     <View style={styles.container}>
@@ -27,12 +48,26 @@ export default function App() {
         <ImageSource 
         placeholderImageSource={PlaceholderImage} 
         selectedImage={selectedImage} />
-        
-     </View>
+                {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
+      {showAppOptions ? (
+             <View style={styles.optionsContainer}>
+             <View style={styles.optionsRow}>
+               <IconButton icon="refresh" label="Reset" onPress={onReset} />
+               <CircleButton onPress={onAddSticker} />
+               <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+             </View>
+           </View>
+      ):(
+     
      <View style={styles.footerContainer}>
       <Button theme='primary' label='choose a photo' onPress={pickImageAsync}/>
-      <Button label='use a photo'/>
+      <Button label='use a photo' onPress={()=>setShowAppOptions(true)} />
      </View>
+     )}  
+     <EmojiPicker  isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+      </EmojiPicker>
       <StatusBar style="auto" />
     </View>
   );
@@ -52,5 +87,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
--- components/button.js
+++ components/button.js
@@ -2,7 +2,7 @@ import { StyleSheet, View, Pressable, Text, onPress } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme,onPress }) {
     if(theme === 'primary'){
             return (
                 <View
@@ -25,7 +25,7 @@ export default function Button({ label, theme }) {
             }
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
